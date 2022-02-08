1d65ac49055d503e96436e3ff3da25b07e5bb62e
batch-05 <nasir.joypurhat@gmail.com>
2022-02-09 03:11:38 +0600

    added

--- js/add.js
+++ js/add.js
@@ -1,16 +1,21 @@
-document.getElementById("lightblue-color").style.color = "lightblue";
 
+//shoes color
+document.getElementById("lightblue-color").style.color = "lightblue";
+// Backpack background color
 document.getElementById("tomato-color").style.backgroundColor = "tomato";
 
+//console.log()
 document.getElementById("first-button").addEventListener("click", function () {
   console.log("Hello World !");
 });
 
+//card radius
 const cards = document.getElementsByClassName("card");
 for (const card of cards) {
   card.style.borderRadius = "30px";
 }
 
+// remove button
 const removeButton=document.getElementsByClassName('buy-now-button');
 // console.log(removeButton);
 for(const remove of removeButton){
@@ -21,3 +26,17 @@ for(const remove of removeButton){
 
 }
  
+//email buttom
+
+document.getElementById('inputremove').addEventListener('keyup',function(e){
+    const deletes =document.getElementById('delete');
+    if(e.target.value=='email'){
+      deletes.removeAttribute('disabled');
+    }
+    else{
+      deletes.setAttribute('disabled',true);
+    }
+    deletes.value='';
+
+  })
+  
